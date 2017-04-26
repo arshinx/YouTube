1212bc675d13af3e14010368679ea7f20b503e3b
Arshin Jain <arshin.jain@outlook.com>
2017-04-26 14:44:11 -0700

    Initialize Cell - Home

--- YouTube/ViewController.swift
+++ YouTube/ViewController.swift
@@ -51,5 +51,12 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
 // New Class
 class VideoCell: UICollectionViewCell {
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
