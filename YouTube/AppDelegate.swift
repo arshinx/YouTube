c7386e9638d020862a51b0be9f55cbd5071b4c6b
Arshin Jain <arshin.jain@outlook.com>
2017-04-27 14:10:10 -0700

    Update UIColor Extension for method return statement addition #2

--- YouTube/AppDelegate.swift
+++ YouTube/AppDelegate.swift
@@ -12,7 +12,7 @@ import UIKit
 // Create Extension to Arrange Colors
 extension UIColor {
     
-    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) {
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
         return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
     }
 }
