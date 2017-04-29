0d5e034131d57cdeb4e256316f7d6c5b3abbd1fb
Arshin Jain <arshin.jain@outlook.com>
2017-04-29 00:59:29 -0700

    MenuCell Class declaration

--- YouTube/MenuBar.swift
+++ YouTube/MenuBar.swift
@@ -73,6 +73,25 @@ class MenuBar: UIView, UICollectionViewDataSource, UICollectionViewDelegate, UIC
     }
 }
 
-class MenuCell : UICollectionViewCell() {
+// MenuCell class
+class MenuCell : UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
     
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
