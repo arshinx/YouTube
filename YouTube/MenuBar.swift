0cec4395f1179cbfe9ee3440b5d788b2d9a3c0f9
Arshin Jain <arshin.jain@outlook.com>
2017-04-28 23:51:52 -0700

    Resolve Compiler Errors - req init coder #10

--- YouTube/MenuBar.swift
+++ YouTube/MenuBar.swift
@@ -15,4 +15,8 @@ class MenuBar: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
