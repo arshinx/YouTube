83dbfedeef5e6dd9444f4d572ed713f8ddac4021
Arshin Jain <arshin.jain@outlook.com>
2017-04-30 08:28:44 -0700

    Restrict More Button to its Original Color - White #17

--- YouTube/ViewController.swift
+++ YouTube/ViewController.swift
@@ -52,7 +52,7 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
         // Create Buttons
         let searchImage = UIImage(named: "search_icon")?.withRenderingMode(.alwaysOriginal)
         let searchBarButtonItem = UIBarButtonItem(image: searchImage, style: .plain, target: self, action: #selector(handleSearch)) // Search Button
-        let moreButton = UIBarButtonItem(image: UIImage(named: "nav_more_icon"), style: .plain, target: self, action: #selector(handleMore)) // More Button
+        let moreButton = UIBarButtonItem(image: UIImage(named: "nav_more_icon")?.withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(handleMore)) // More Button
         // Right Bar Buttons
         navigationItem.rightBarButtonItems = [moreButton, searchBarButtonItem] // Add Buttons
     }
