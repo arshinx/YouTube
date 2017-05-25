d5a799dbb345ee2eae8a27a6b79080f68807cf5e
Arshin Jain <arshin.jain@outlook.com>
2017-05-25 13:40:18 -0700

    Invert declaration types - imageCache #25

--- YouTube/Extensions.swift
+++ YouTube/Extensions.swift
@@ -40,7 +40,7 @@ extension UIView {
 }
 
 // Create an image cache
-let imageCache = NSCache<UIImage, NSString>()
+let imageCache = NSCache<NSString, UIImage>()
 
 // Extend UIImage View
 extension UIImageView {
